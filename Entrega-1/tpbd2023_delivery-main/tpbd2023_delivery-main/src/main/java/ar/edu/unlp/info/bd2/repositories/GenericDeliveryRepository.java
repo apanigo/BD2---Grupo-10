578099f607b280ca6549302754d5ec463d4a2cdb
Paula Vaccaro <paulavaccaro1@gmail.com>
2023-04-14 10:42:08 -0300

    Generic getters con HQL en lugar de Criteria

--- Entrega-1/tpbd2023_delivery-main/tpbd2023_delivery-main/src/main/java/ar/edu/unlp/info/bd2/repositories/GenericDeliveryRepository.java
+++ Entrega-1/tpbd2023_delivery-main/tpbd2023_delivery-main/src/main/java/ar/edu/unlp/info/bd2/repositories/GenericDeliveryRepository.java
@@ -8,6 +8,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class GenericDeliveryRepository {
@@ -22,29 +23,19 @@ public abstract class GenericDeliveryRepository {
 	}
 	
 	protected <T> Optional<T> getOptionalByProperty(String propertyName, Object propertyValue, Class<T> entityClass) {
-		CriteriaBuilder builder = this.sessionFactory.getCurrentSession().getCriteriaBuilder();
-		// Crea un objeto CriteriaBuilder para la entityClass
-	    CriteriaQuery<T> criteria = builder.createQuery(entityClass);
-	    // Selecciona todas las entidades del tipo entityClass
-        criteria.select(criteria.from(entityClass));
-        // Filtra las entidades según el valor (propertyValue) de la propiedad (propertyName)
-        criteria.where(builder.equal(criteria.from(entityClass).get(propertyName), propertyValue));
-        // Ejecuta query y obtiene la lista de resultados
-        List<T> resultList = this.sessionFactory.getCurrentSession().createQuery(criteria).getResultList();
-        // Si la lista está vacía retorna un opcional vacío
+		String queryString = "FROM " + entityClass.getName() + " WHERE " + propertyName + " = :propertyValue";
+	    Query<T> query = this.sessionFactory.getCurrentSession().createQuery(queryString, entityClass);
+	    query.setParameter("propertyValue", propertyValue);
+	    List<T> resultList = query.getResultList();
 	    return resultList.isEmpty() ? Optional.empty() : Optional.of(resultList.get(0));
     }
 
 	protected <T> T getClassByProperty(String propertyName, Object propertyValue, Class<T> entityClass) {
-        CriteriaBuilder builder = this.sessionFactory.getCurrentSession().getCriteriaBuilder();
-        // Crea un objeto CriteriaBuilder para la entityClass
-        CriteriaQuery<T> criteria = builder.createQuery(entityClass);
-        // Selecciona todas las entidades del tipo entityClass
-        criteria.select(criteria.from(entityClass));
-        // Filtra las entidades según el valor (propertyValue) de la propiedad (propertyName)
-        criteria.where(builder.equal(criteria.from(entityClass).get(propertyName), propertyValue));
-        // Ejecuta la query y retorna el resultado único
-        return sessionFactory.getCurrentSession().createQuery(criteria).getSingleResult();
+		String queryString = "FROM " + entityClass.getName() + " WHERE " + propertyName + " = :propertyValue";
+	    Query<T> query = this.sessionFactory.getCurrentSession().createQuery(queryString, entityClass);
+	    query.setParameter("propertyValue", propertyValue);
+	    List<T> resultList = query.getResultList();
+	    return resultList.isEmpty() ? null : resultList.get(0);
     }
 	
 	protected <T> Optional<T> getOptionalById(Serializable id, Class<T> entityClass) {
